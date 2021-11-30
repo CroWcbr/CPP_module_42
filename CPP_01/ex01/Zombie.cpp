f31f3a824491647bc2f0ac53bc90d1b0b735f801
Vitaliy <94758944+CroWcbr@users.noreply.github.com>
2021-12-01 00:27:51 +0300

    upload ex01

---
+++ CPP_01/ex01/Zombie.cpp
@@ -0,0 +1,25 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name): _name(name)
+{
+}
+
+Zombie::~Zombie()
+{
+	std::cout << "<" << getName() << "> died" << std::endl;
+}
+
+void Zombie::announce(void) const
+{
+	std::cout << "<" << getName() << "> BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+std::string	Zombie::getName( void ) const
+{
+	return (this->_name);
+}
+
+void Zombie::setName(std::string name)
+{
+	this->_name = name;
+}
---
+++ CPP_01/ex01/Zombie.hpp
@@ -0,0 +1,23 @@
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+# include <string>
+
+class	Zombie
+{
+private:
+	std::string _name;
+	
+public:
+	Zombie(std::string name = "noname");
+	~Zombie();
+	void		announce(void) const;
+	std::string	getName( void ) const;
+	void		setName(std::string name);
+};
+
+Zombie* zombieHorde(int N, std::string name);
+
+#endif
