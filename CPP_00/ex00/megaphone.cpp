a16a78a96629bbd0143b2f97c4be7be0dbb34dc9
Vitaliy <94758944+CroWcbr@users.noreply.github.com>
2021-12-16 12:53:17 +0300

    update
    add space

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -5,7 +5,7 @@ static void	print_str(char *str)
 {
 	while (*str)
 	{
-		std:: cout << (char)toupper(*str);
+		std::cout << (char)toupper(*str);
 		str++;
 	}
 }
@@ -15,13 +15,17 @@ int	main(int argc, char **argv)
 	int i;
 	
 	if (argc == 1)
-		std:: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
 		i = 0;
 		while (++i < argc)
+		{
 			print_str(argv[i]);
-		std:: cout <<std::endl;
+			if (i + 1 < argc)
+				std::cout << " ";
+		}
+		std::cout << std::endl;
 	}
 	return (0);
 }
